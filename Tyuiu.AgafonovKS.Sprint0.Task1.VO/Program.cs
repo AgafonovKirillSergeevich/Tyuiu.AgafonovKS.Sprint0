a89c58bf36cb0dc256f7c0746c62675255011d66
Кирилл Агафонов <Kirya_agafonov_2002@mail.ru>
2023-09-11 21:11:28 +0500

    Task1

---
+++ Tyuiu.AgafonovKS.Sprint0.Task1.VO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AgafonovKS.Sprint0.Task1.VO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Kirill";
+            int age = 21;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " год!");
+            Console.ReadKey();
+        }
+    }
+}
